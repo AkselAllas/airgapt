bf93132bb07b39310f8afaec0bdfeeaacb3c741e
Kali <kali@kali>
2021-12-01 09:23:29 -0500

    Change remote proxy test for less-flaky local test

--- src/localProxySetup.sh
+++ src/localProxySetup.sh
@@ -27,15 +27,15 @@ ensure_ssh_socks_proxy_is_up(){
 }
 
 ensure_remote_ssh_forward_is_up(){
-  SSH_FORWARD_UP=$(ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} "sudo ss -tulpna | grep 127.0.0.1:${TARGET_PORT} | wc -l")
+  SSH_FORWARD_UP=$(sudo ss -tulpna | grep 127.0.0.1:${SOCKS_PORT} | wc -l)
   if [ $SSH_FORWARD_UP = 1 ]; then
     success "[+] Remote SSH already forwarded"
   else
     info "[ ] Creating remote SSH forward to ${TARGET_USER}@${TARGET} -R${TARGET_PORT}:localhost:${SOCKS_PORT}";
     set -x
     ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} -f -N -R${TARGET_PORT}:localhost:${SOCKS_PORT} 2>/dev/null
+    SSH_FORWARD_UP=$(sudo ss -tulpna | grep 127.0.0.1:${SOCKS_PORT} | wc -l)
     set +x
-    SSH_FORWARD_UP=$(ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} "sudo ss -tulpna | grep 127.0.0.1:${TARGET_PORT} | wc -l")
     if [ $SSH_FORWARD_UP = 1 ]; then
       success "[+] Remote SSH forward successfully made"
     else
