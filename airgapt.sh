67effed07a6a382dc28f1c1051c60a2e4ca3320f
Aksel Allas <aksel.allas@easypark.net>
2021-12-02 19:04:48 +0200

    Refactor spread out files to one for better deployability

--- airgapt.sh
+++ airgapt.sh
@@ -2,24 +2,197 @@
 set -e
 set -u
 
-#### VARIABLES TO CHANGE ####
+#### VARIABLES TO CHANGE ###################################################################
 SOCKS_PORT=44444
 LOCAL_USER="kali"
-SSH_KEY_ARGUMENT=""
-TARGET="changeme"
+TARGET="changeme.domain"
 TARGET_USER="ubuntu"
 TARGET_PORT="6666"
-#############################
+SSH_KEY_ARGUMENT="" #in format of "-i ~/.ssh/custom_key"
+############################################################################################
+
+#Next lines are function definitions for ~200 lines.
+#Functions get called at main function at end of script
+#### UTILITY FUNCTIONS #####################################################################
+C=$(printf '\033')
+YELLOW="${C}[1;33m"
+RED="${C}[1;31m"
+BLUE="${C}[1;34m"
+GREEN="${C}[1;32m"
+NC="${C}[0m"
 
 error(){
   printf "${RED}$1\n"
   exit
 }
+info(){
+  printf "${YELLOW}$1$NC\n"
+}
+success(){
+  printf "${GREEN}$1$NC\n"
+}
+print_title(){
+  printf ${BLUE}"-=//=- $GREEN$1 ${BLUE}-=//=-$NC\n"
+}
 
-source src/utilities.sh
+ensure_root(){
+  if ([ -f /usr/bin/id ] && [ "$(/usr/bin/id -u)" -eq "0" ]) || [ "$(whoami 2>/dev/null)" = "root" ]; then
+    IAMROOT="1"
+  else
+    error "You need to run this as root"
+  fi
+}
+############################################################################################
+
+
+#### DEPENDENCY SETUP FUNCTIONS ############################################################
+does_valid_config_line_exist(){
+  STRING=$1
+  FILE=$2
+  grepRES=$(grep "$STRING" "$FILE" | grep --perl-regexp --invert-match '(?:^;)|(?:^\s*/\*.*\*/)|(?:^\s*#|//|\*)')
+  if [ ${#grepRES} -ge 1 ] ; then
+    # true
+    return 0
+  else
+    # false
+    return 1
+  fi
+}
+ensure_ssh_config_line_exists(){
+  STRING=$1
+  FILE=$2
+  if does_valid_config_line_exist "$STRING" "$FILE";
+  then
+    success "[+] sshd_config has $STRING";
+  else
+    info "[ ] adding $STRING";
+    set -x
+    echo "$STRING" >> "$FILE";
+    set +x
+    if does_valid_config_line_exist "$STRING" "$FILE";
+    then
+      success "[+] sshd_config now has $STRING";
+      RESTART_SSH="true"
+    else
+      error "[-] Failed to setup $FILE"
+    fi
+  fi
+}
+install_openssh_server(){
+  if dpkg --list | grep -q openssh-server
+  then
+    success "[+] Openssh-server already installed";
+  else
+    info "[ ] Installing openssh-server";
+    set -x
+    apt-get install -y openssh-server
+    set +x
+    success "[+] Openssh-server install succeeded";
+  fi
+}
+test_sshd_service(){
+  if systemctl status ssh.service | grep -q "masked"; then
+      error "[-] SSH service is masked, you need to unmask it.
+      e.g run:
+        systemctl unmask ssh.service
+        systemctl enable ssh.service
+        systemctl start ssh.service
+        systemctl status ssh.service
+      "
+  fi
+  if systemctl status ssh.service | grep -q "Active: active"; then
+      success "[+] ssh server working"
+  else
+      info "[+] restarted ssh.service"
+      systemctl restart ssh.service
+      if systemctl status ssh.service | grep -q "Active: active"; then
+          success "[+] ssh server working"
+      else
+         error "[-] ssh.service isn't active"
+      fi
+  fi
+}
+setup_sshd_config(){
+  RESTART_SSH="false"
+  ensure_ssh_config_line_exists 'AllowTcpForwarding yes' '/etc/ssh/sshd_config'
+  ensure_ssh_config_line_exists 'GatewayPorts yes' '/etc/ssh/sshd_config'
+  if [ $RESTART_SSH = "true" ] ; then
+    info "[ ] Restarting SSH"
+    set -x
+    systemctl restart ssh.socket
+    systemctl start ssh.service
+    set +x
+  fi
+  test_sshd_service
+}
+############################################################################################
+
+
+#### LOCAL PROXY FUNCTIONS #################################################################
+test_ssh_socks_proxy(){
+  if curl -sL --fail --socks5 localhost:"${SOCKS_PORT}" http://google.com -o /dev/null; then
+    success "[+] SOCKS proxy working correctly"
+  else
+    error "[ ] SOCKS proxy setup failed"
+  fi
+}
+
+create_ssh_socks_proxy(){
+  info "[ ] Creating local SOCKS proxy to ${LOCAL_USER}@localhost (Allows dynamic outgoing port from airgapped server)";
+  set -x
+  ssh -f -N -D${SOCKS_PORT} ${SSH_KEY_ARGUMENT} ${LOCAL_USER}@localhost
+  set +x
+  test_ssh_socks_proxy
+}
+
+ensure_ssh_socks_proxy_is_up(){
+  if curl -sL --fail --socks5 localhost:"${SOCKS_PORT}" http://google.com -o /dev/null; then
+    success "[+] SOCKS proxy working correctly"
+  else
+    create_ssh_socks_proxy
+  fi
+}
+
+ensure_remote_ssh_forward_is_up(){
+  SSH_FORWARD_UP=$(ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} "sudo ss -tulpna | grep 127.0.0.1:${TARGET_PORT} | grep LISTEN | wc -l")
+  if [ $SSH_FORWARD_UP = 1 ]; then
+    success "[+] Remote SSH already forwarded"
+  else
+    info "[ ] Creating remote SSH forward to ${TARGET_USER}@${TARGET} -R${TARGET_PORT}:localhost:${SOCKS_PORT}";
+    set -x
+    ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} -f -N -R${TARGET_PORT}:localhost:${SOCKS_PORT} 2>/dev/null
+    set +x
+    SSH_FORWARD_UP=$(ssh ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} "sudo ss -tulpna | grep 127.0.0.1:${TARGET_PORT} | grep LISTEN | wc -l")
+    if [ $SSH_FORWARD_UP = 1 ]; then
+      success "[+] Remote SSH forward successfully made"
+    else
+      error "[-] Remote SSH forward to ${TARGET_USER}@${TARGET} -R${TARGET_PORT}:localhost:${SOCKS_PORT} failed"
+    fi
+  fi
+}
+############################################################################################
+
+
+#### REMOTE PROXY FUNCTIONS ################################################################
+ensure_remote_server_has_proxy_config(){
+ssh -t ${SSH_KEY_ARGUMENT} ${TARGET_USER}@${TARGET} <<:
+sudo su
+cat <<EOT > /etc/apt/apt.conf.d/proxy.conf 
+Acquire {
+  HTTP::proxy "socks5h://127.0.0.1:$TARGET_PORT";
+  HTTPS::proxy "socks5h://127.0.0.1:$TARGET_PORT";
+}
+EOT
+:
+}
+############################################################################################
+
+
+#### MAIN ##################################################################################
 ensure_root
 print_title airgapt
-
-source src/setupDependencies.sh
-source src/localProxySetup.sh
-source src/remoteProxySetup.sh
+install_openssh_server
+setup_sshd_config
+ensure_ssh_socks_proxy_is_up
+ensure_remote_ssh_forward_is_up
+ensure_remote_server_has_proxy_config
